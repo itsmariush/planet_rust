d9510ad6f69851aca928d3eb09d8757f51908709
x <x@mail.com>
2022-09-16 13:28:18 +0200

    Two body trajectory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::convert::TryInto;
 use std::f32::consts::PI;
 
-use bevy::prelude::*;
 use bevy::input::mouse::*;
+use bevy::prelude::*;
 use bevy::render::camera::Projection;
 use bevy_inspector_egui::WorldInspectorPlugin;
 use bevy_inspector_egui_rapier::InspectableRapierPlugin;
@@ -11,7 +11,6 @@ extern crate peroxide;
 use peroxide::fuga::*;
 use peroxide::numerical::ode;
 
-const _G: f32 = 1f32;//6.67259e-20;
 
 #[derive(Component)]
 struct PanOrbitCamera {
@@ -22,10 +21,10 @@ struct PanOrbitCamera {
 
 impl Default for PanOrbitCamera {
     fn default() -> Self {
-        PanOrbitCamera { 
+        PanOrbitCamera {
             focus: Vec3::ZERO,
-            radius: 5.0, 
-            upside_down: false, 
+            radius: 5.0,
+            upside_down: false,
         }
     }
 }
@@ -41,7 +40,7 @@ fn pan_orbit_camera(
     mut ev_scroll: EventReader<MouseWheel>,
     input_mouse: Res<Input<MouseButton>>,
     mut query: Query<(&mut PanOrbitCamera, &mut Transform, &Projection)>,
-){
+) {
     let orbit_button = MouseButton::Right;
     let pan_button = MouseButton::Middle;
 
@@ -49,7 +48,7 @@ fn pan_orbit_camera(
     let mut pan = Vec2::ZERO;
     let mut scroll = 0.0;
     let mut orbit_button_changed = false;
-    
+
     if input_mouse.pressed(orbit_button) {
         for ev in ev_motion.iter() {
             rotation_move += ev.delta;
@@ -69,7 +68,6 @@ fn pan_orbit_camera(
     }
 
     for (mut pan_orbit, mut transform, projection) in query.iter_mut() {
-
         if orbit_button_changed {
             let up = transform.rotation * Vec3::Y;
             pan_orbit.upside_down = up.y <= 0.0;
@@ -81,7 +79,11 @@ fn pan_orbit_camera(
             let window = get_primary_window_size(&windows);
             let delta_x = {
                 let delta = rotation_move.x / window.x * PI * 2.0;
-                if pan_orbit.upside_down { -delta } else { delta }
+                if pan_orbit.upside_down {
+                    -delta
+                } else {
+                    delta
+                }
             };
             let delta_y = rotation_move.y / window.y * PI;
             let yaw = Quat::from_rotation_y(-delta_x);
@@ -96,7 +98,7 @@ fn pan_orbit_camera(
             }
             let right = transform.rotation * Vec3::X * -pan.x;
             let up = transform.rotation * Vec3::Y * pan.y;
-            let translation = (right+up) * pan_orbit.radius;
+            let translation = (right + up) * pan_orbit.radius;
             pan_orbit.focus += translation;
         } else if scroll.abs() > 0.0 {
             any = true;
@@ -107,7 +109,8 @@ fn pan_orbit_camera(
 
         if any {
             let rot_matrix = Mat3::from_quat(transform.rotation);
-            transform.translation = pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
+            transform.translation =
+                pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
         }
     }
 }
@@ -119,7 +122,9 @@ fn get_primary_window_size(windows: &Res<Windows>) -> Vec2 {
 }
 
 // https://towardsdatascience.com/use-python-to-create-two-body-orbits-a68aed78099c
-fn gravity_system(mut query: Query<(&Name, &mut Velocity, &Transform, &ReadMassProperties), With<Planet>>) {
+fn gravity_system(
+    mut query: Query<(&Name, &mut Velocity, &Transform, &ReadMassProperties), With<Planet>>,
+) {
     let center = Vec3::ZERO;
     let sun_mass = 1000.0;
     let sun_grav_param = {
@@ -145,24 +150,29 @@ fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut rapier_config: ResMut<RapierConfiguration>
+    mut rapier_config: ResMut<RapierConfiguration>,
 ) {
     rapier_config.gravity = Vec3::ZERO;
 
     // Plane
     commands
         .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane {size: 100.0})),
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 100.0 })),
             material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
             ..default()
         })
-    .insert(Transform::from_xyz(0.0, -2.0, 0.0));
+        .insert(Transform::from_xyz(0.0, -2.0, 0.0));
 
     // Sun
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Icosphere {radius: 1.0, subdivisions: 6})),
-        material: materials.add(Color::rgb(0.990, 0.945, 0.455).into()),
-        ..default()})
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                radius: 1.0,
+                subdivisions: 6,
+            })),
+            material: materials.add(Color::rgb(0.990, 0.945, 0.455).into()),
+            ..default()
+        })
         .insert(RigidBody::Dynamic)
         .insert(Collider::ball(1.0))
         .insert(ColliderMassProperties::Mass(100.0))
@@ -171,12 +181,17 @@ fn setup_scene(
         })
         .insert(Name::new("Sun"))
         .insert(Sun);
-    
+
     // Earth
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Icosphere {radius: 1.0, subdivisions: 6})),
-        material: materials.add(Color::rgb(0.0, 0.0, 1.0).into()),
-        ..default()})
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                radius: 1.0,
+                subdivisions: 6,
+            })),
+            material: materials.add(Color::rgb(0.0, 0.0, 1.0).into()),
+            ..default()
+        })
         .insert(RigidBody::Dynamic)
         .insert(Collider::ball(1.0))
         .insert(ColliderMassProperties::Mass(1.0))
@@ -186,55 +201,62 @@ fn setup_scene(
         .insert(Name::new("Earth"))
         .insert(Planet);
 
-    let com : Vec3 = calc_com(1000f32, 200f32, Vec3::ZERO, Vec3::new(4.0, 0.0, 4.0));
+    let com: Vec3 = calc_com(1000f32, 200f32, Vec3::ZERO, Vec3::new(4.0, 0.0, 4.0));
     println!("Center of Mass: {com}");
 
     // Center of Mass
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Icosphere {radius: 0.5, subdivisions: 6})),
-        material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
-        ..default()})
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                radius: 0.5,
+                subdivisions: 6,
+            })),
+            material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
+            ..default()
+        })
         .insert(Transform::from_translation(com))
         .insert(ReadMassProperties::default())
         .insert(Velocity::default())
         .insert(Name::new("COM"))
         .insert(Planet);
 
+    let r_mag = 15f64;
+    let v_mag = (MU / r_mag).sqrt();
     let mut ode_test = ExplicitODE::new(f);
-    let init_state : ode::State<f64> = ode::State::new(
-            0.0,
-            vec![0.0, 0.0, 0.0, 15.0, 1.0, 1.0],
-            vec![0.0, 0.0, 0.0, 0.0, 10.0, 0.0]
-        );
+    println!("V_Mag: {v_mag}");
+    let init_state: ode::State<f64> = ode::State::new(
+        0.0,
+        vec![r_mag as f64, 0.0, 0.0, 0.0, 0.0, v_mag as f64],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    );
     let result = ode_test
         .set_initial_condition(init_state)
         .set_method(ExMethod::RK4)
-        .set_step_size(0.0001f64)
-        .set_times(100)
+        .set_step_size(1.0f64)
+        .set_times(350)
         .integrate();
 
     println!("{result}");
 
     for n in 0..result.row {
         let row = result.row(n);
-
-        commands.spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {radius: 0.2, subdivisions: 6})),
-            material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
-            ..default()})
-            .insert(Transform::from_xyz(row[1] as f32, row[2] as f32, row[3] as f32))
+        commands
+            .spawn_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Icosphere {
+                    radius: 0.1,
+                    subdivisions: 3,
+                })),
+                material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                ..default()
+            })
+            .insert(Transform::from_xyz(
+                row[1] as f32,
+                row[2] as f32,
+                row[3] as f32,
+            ))
             .insert(ReadMassProperties::default())
             .insert(Velocity::default())
             .insert(Name::new("Trajectory1"));
-            
-        commands.spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {radius: 0.2, subdivisions: 6})),
-            material: materials.add(Color::rgb(0.0, 0.0, 1.0).into()),
-            ..default()})
-            .insert(Transform::from_xyz(row[4] as f32, row[5] as f32, row[6] as f32))
-            .insert(ReadMassProperties::default())
-            .insert(Velocity::default())
-            .insert(Name::new("Trajectory2"));
     }
 
     commands.spawn_bundle(PointLightBundle {
@@ -247,44 +269,52 @@ fn setup_scene(
         ..default()
     });
 
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(-4.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    })
-    .insert(PanOrbitCamera::default());
+    commands
+        .spawn_bundle(Camera3dBundle {
+            transform: Transform::from_xyz(-4.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        })
+        .insert(PanOrbitCamera::default());
 }
 
+const _G: f32 = 1f32; //6.67259e-20;
+const M1: f64 = 333.0;
+const M2: f64 = 1.0;
+const MU: f64 = (M1*M2)/(M1+M2);
 fn f(st: &mut ode::State<f64>, _: &NoEnv) {
-    let value = &mut st.value;
-    let derive = &mut st.deriv; 
-    
-    let r1 = Vec3::new(value[0] as f32, value[1] as f32, value[2] as f32);
-    let r2 = Vec3::new(value[3] as f32, value[4] as f32, value[5] as f32);
+    let value = &st.value;
+    let derive = &mut st.deriv;
+    // mu = (m1*m2)/(m1+m2)
 
-    let r_mag = (r2 - r1).normalize();
+    let mut r: Vec3 = Vec3::ZERO;
+    let mut v: Vec3 = Vec3::ZERO;
+    if let [rx, ry, rz] = value[0..3] {
+        if let [vx, vy, vz] = value[3..] {
+            r = Vec3::new(rx as f32, ry as f32, rz as f32);
+            v = Vec3::new(vx as f32, vy as f32, vz as f32);
+        }
+    }
+    println!("R: {r} V: {v}");
+    let r_norm = r.length();
+    println!("R_Norm: {r_norm}");
+   
+    let a  = r * MU as f32 / r_norm.powf(3.0);
+    let ax = -r.x as f64 * MU / r_norm.powi(3) as f64;
+    let ay = -r.y as f64 * MU / r_norm.powi(3) as f64;
+    let az = -r.z as f64 * MU / r_norm.powi(3) as f64;
+    println!("AA: {a}");
+    println!("A: {ax} {ay} {az}");
 
-    // X1 = G * m2 * ((X2-X1) / r^3)
-    let m2 = 0.1f32;
-    let m1 = 33.3f32;
-    let r_mag3 = r_mag.powf(3.0);
-    let nx1 = _G * m2 * ((r2.x - r1.x) / r_mag3.x);
-    let ny1 = _G * m2 * ((r2.y - r1.y) / r_mag3.y);
-    let nz1 = _G * m2 * ((r2.z - r1.z) / r_mag3.z);
- 
-    let nx2 = _G * m1 * ((r1.x - r2.x) / r_mag3.x);
-    let ny2 = _G * m1 * ((r1.y - r2.y) / r_mag3.y);
-    let nz2 = _G * m1 * ((r1.z - r2.z) / r_mag3.z);
+    derive[0] = v.x as f64;
+    derive[1] = v.y as f64;
+    derive[2] = v.z as f64;
+    derive[3] = if ax.is_nan() { 0f64 } else { ax as f64};
+    derive[4] = if ay.is_nan() { 0f64 } else { ay as f64};
+    derive[5] = if az.is_nan() { 0f64 } else { az as f64};
 
-    derive[0] = nx1 as f64;
-    derive[1] = ny1 as f64;
-    derive[2] = nz1 as f64;
-    derive[3] = nx2 as f64;
-    derive[4] = ny2 as f64;
-    derive[5] = nz2 as f64;
 }
 
 fn main() {
-
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
